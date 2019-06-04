df9ad58dd76ad15e160edc9e19647073996a3f74
alumno <alumno@utnfra.com.ar>
2019-06-03 22:26:13 -0300

    first commit

---
+++ WindowsFormsApp10/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp10
+{
+    public partial class Form1 : Form
+    {
+        List<Provincia> provincias;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            provincias = new List<Provincia>();
+            SqlConnection cn = new SqlConnection(
+                Properties.Settings.Default.CadenaASQL);
+
+            SqlCommand cm = new SqlCommand("Select id, nombre from provincias", cn);
+            cm.Connection = cn;
+            cm.CommandText = "Select id, nombre from provincias";
+            cn.Open();
+
+            SqlDataReader dr =  cm.ExecuteReader();
+            
+            while( dr.Read())
+            {
+                 provincias.Add(new Provincia((int)((decimal)dr["id"]), 
+                     dr["nombre"].ToString()));
+            }
+
+            cn.Close();
+
+            this.cmbPcia.DataSource = provincias;
+
+            
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (cmbPcia.SelectedIndex > -1) {
+                SqlConnection cn = new SqlConnection(
+                   Properties.Settings.Default.CadenaASQL);
+
+                SqlCommand cm = new SqlCommand();
+                cm.Connection = cn;
+                
+
+                string consulta = "insert into localidades (ProvinciaId, nombre ) values ( ";
+                consulta += ((Provincia)cmbPcia.SelectedItem).Id.ToString() + ",'" + txtLocalidad.Text + "')";
+                cn.Open();
+                cm.CommandText = consulta;
+
+                cm.ExecuteNonQuery();
+
+
+
+                cn.Close();
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp10/Provincia.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp10
+{
+    public class Provincia
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        
+        public Provincia(int id, string nombre)
+        {
+            this.id = id;
+            this.nombre = nombre;
+        }
+
+        public override string ToString()
+        {
+            return this.nombre;
+        }
+
+    }
+}
